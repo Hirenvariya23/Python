4a5375e321c64d6ac8d4b420fcdf1e95f9af875e
hardik-1998 <hardikvariya98@gmail.com>
2024-06-30 15:56:51 +0530

    day1 python practice

---
+++ 100DaysOfCode/Day1.py
@@ -0,0 +1,12 @@
+# python version 3.11.4
+print("Hello "'python!!'"")  # using ' with "
+print("Hiren \"Variya\"") # printing "
+print("Hiren \'Variya\'") # printing "
+print("Hello\n World\n")  # \n newline
+
+print("Hello " + input("What is your name?\n"))    #string concatenation
+num1 = int(input("Enter 1st no:"))
+num2 = int(input("Enter 2nd no:"))
+num3 = int(input("Enter 3rd no:"))
+noOfLetters = len(input("Enter Your Country: "))
+print("Number of letters in your country = " + str(noOfLetters))
